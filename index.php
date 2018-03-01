144b3ca44a55d37e151834c66b441dd5f0a2f6f7
Bianca Hernandez <bihernandez@csumb.edu>
2018-03-01 20:48:18 +0000

    Added styles tag>

--- index.php
+++ index.php
@@ -7,8 +7,15 @@
     <head>
         <title>Silver Jack Game</title>
         <meta charset = "utf-8" />
+        
+        <style>
+            @import url("css/styles.css");
+        </style>
     </head>
     <body>
+        <center>
+        <h1>Silver Jack Game</h1>
+        </center>
         <div id = "main">
             <?=playCards()?>
             <form>

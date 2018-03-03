6ab0f3c05a70160fc5b1e4eceb1718250f6c3e3b
Eli Manzo <elmanzo@csumb.edu>
2018-03-03 00:20:02 +0000

    Adding better allignment

--- index.php
+++ index.php
@@ -7,17 +7,12 @@
     <head>
         <title>Silver Jack Game</title>
         <meta charset = "utf-8" />
-        
         <style>
             @import url("css/styles.css");
         </style>
     </head>
     <body>
-        <center>
-        
-        </center>
         <div id = "main">
-            <h1>Silver Jack Game</h1>
             <?=playCards()?>
             <form>
                 <input type="submit" value="Play!"/>
